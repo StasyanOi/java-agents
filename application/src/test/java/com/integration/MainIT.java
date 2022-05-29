04b8469942ac5247424fc1429499d07ece5a658f
StasyanOi <iluvstrwrs@mail.ru>
2022-05-29 18:17:47 +0300

    added logging

--- application/src/test/java/com/integration/MainIT.java
+++ application/src/test/java/com/integration/MainIT.java
@@ -5,6 +5,8 @@ import org.awaitility.Duration;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,17 +14,23 @@ import java.io.InputStreamReader;
 
 class MainIT {
 
+    private static final Logger logger = LoggerFactory.getLogger(MainIT.class);
+
     @Test
     @EnabledOnOs(OS.LINUX)
     void runJavaAgents() throws IOException {
+        logger.info("Started test");
         Process application = Runtime.getRuntime().exec("java -javaagent:../static-java-agent/target/static-java-agent-1.0.jar -jar ./target/application-SNAPSHOT-1.0.jar");
         Process dynamicAgentLoading = Runtime.getRuntime().exec("java -jar ../dynamic-java-agent/loader-application/target/loader-application-1.0.jar");
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(application.getInputStream()))) {
             Awaitility.await().atMost(Duration.FIVE_SECONDS).until(() -> bufferedReader.lines()
                     .anyMatch(log -> log.contains("Java agent loaded")));
+            logger.info("Static agent loaded");
             Awaitility.await().atMost(Duration.FIVE_SECONDS).until(() -> bufferedReader.lines()
                     .anyMatch(log -> log.contains("Dynamic agent attached")));
+            logger.info("Dynamic agent loaded");
         } finally {
+            logger.info("Destroying processes");
             dynamicAgentLoading.destroy();
             application.destroy();
         }
